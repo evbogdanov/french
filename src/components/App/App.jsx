008cc66929ef673971470ce779b99db75a668512
Ev Bogdanov <evbogdanov.com@gmail.com>
2018-10-02 02:02:21 +0300

    Prepare main components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import Phrases from '../Phrases/Phrases';
+import Words from '../Words/Words';
+import About from '../About/About';
 
 class App extends Component {
   render() {
     return (
-      <div className="App"></div>
+      <div className="App">
+        <Phrases />
+        <Words />
+        <About />
+      </div>
     );
   }
 }
---
+++ src/components/Phrases/Phrases.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Phrases = (props) => {
+  return (
+    <div>Phrases</div>
+  );
+};
+
+export default Phrases;
