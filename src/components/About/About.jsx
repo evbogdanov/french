b2cd95f79c37cdb11a332c62c6d7591b2caf1942
Ev Bogdanov <evbogdanov.com@gmail.com>
2018-10-09 16:53:33 +0300

    About: initial text on the page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,10 +3,11 @@ import Heading from '../Heading/Heading';
 
 const About = (props) => {
   return (
-    <>
+    <div className="About">
       <Heading>About</Heading>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    </>
+      <p className="lead">What happens when a web developer decides to learn a new language? Well, he creates an app for it!</p>
+      <p className="lead">My name is Ev. I am a French learner and the author of the app. Sooner or later I'll fill this page up with meaningful stuff.</p>
+    </div>
   );
 };
 
