54dcfb6b32982395d70259ccc88c8247abf061a5
Ev Bogdanov <evbogdanov.com@gmail.com>
2018-11-18 16:34:55 +0300

    Show counters on About page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Heading from '../Heading/Heading';
+import * as api from '../../api';
 
-const About = (props) => {
-  return (
-    <div className="About">
-      <Heading>About</Heading>
-      <p className="lead">What happens when a web developer decides to learn a new language? Well, he creates an app for it!</p>
-      <p className="lead">My name is Ev. I am a French learner and the author of the app. Sooner or later I'll fill this page up with meaningful stuff.</p>
-    </div>
-  );
-};
+class About extends Component {
+  state = {
+    // Why counters are strings:
+    // https://github.com/brianc/node-postgres/issues/378
+    countWords: '0',
+    countPhrases: '0',
+  }
+
+  componentDidMount() {
+    api.get('/v1/stats/count')
+      .then(res => {
+        const count = res.data.data;
+        this.setState({
+          countWords: count.words,
+          countPhrases: count.phrases,
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  render() {
+    return (
+      <div className="About">
+        <Heading>About</Heading>
+        <p className="lead">What happens when a web developer decides to learn a new language? Well, he creates an app for it!</p>
+        <p className="lead">My name is Ev. I am a French learner and the author of the app. Sooner or later I'll fill this page up with meaningful stuff.</p>
+        <hr />
+        <p className="lead">Words: {this.state.countWords}</p>
+        <p className="lead">Phrases: {this.state.countPhrases}</p>
+      </div>
+    );
+  }
+}
 
 export default About;
