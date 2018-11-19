d9d3a716a1033a279cefbf8ea55a1ecd5c5a5474
Ev Bogdanov <evbogdanov.com@gmail.com>
2018-11-20 02:07:28 +0300

    Rewrite About page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Heading from '../Heading/Heading';
+import { Link } from 'react-router-dom';
 import * as api from '../../api';
 
 class About extends Component {
@@ -25,14 +26,45 @@ class About extends Component {
   }
 
   render() {
+    const wordsLink = (
+      <Link to="/words">
+        {this.state.countWords} words
+      </Link>
+    );
+
+    const phrasesLink = (
+      <Link to="/">
+        {this.state.countPhrases} phrases
+      </Link>
+    );
+
     return (
       <div className="About">
-        <Heading>About</Heading>
-        <p className="lead">What happens when a web developer decides to learn a new language? Well, he creates an app for it!</p>
-        <p className="lead">My name is Ev. I am a French learner and the author of the app. Sooner or later I'll fill this page up with meaningful stuff.</p>
-        <hr />
-        <p className="lead">Words: {this.state.countWords}</p>
-        <p className="lead">Phrases: {this.state.countPhrases}</p>
+        <div className="row">
+          <div className="col-md-8">
+            <Heading>Bonjour!</Heading>
+            <p>
+              I'm <a href="https://github.com/evbogdanov">Ev</a>.
+              And this is the app I've been working on recently.
+              It helps me to learn the French language.
+            </p>
+            <p>
+              How does it work? Well, it's kind of my own dictionary.
+              I just write down new words and phrases.
+            </p>
+            <p>
+              I tried to use <a href="https://medium.com/@evguenie">Medium</a> for this
+              purpose but quickly discovered its limitations. I wanted something
+              better, something I can fully customize to my needs. Et voilà!
+            </p>
+            <p>
+              Right now in my dictionary {wordsLink} and {phrasesLink}.
+            </p>
+          </div>
+          <div className="col-md-4 About__image">
+            <img alt="Eiffel Tower" className="img-fluid" src="/img/eiffel.png" />
+          </div>
+        </div>
       </div>
     );
   }
