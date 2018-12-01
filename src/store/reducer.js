5d18b1081b80e8cc5d839fa95075ab8e2866d51e
Ev Bogdanov <evbogdanov.com@gmail.com>
2018-12-02 00:40:41 +0300

    Refactor reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,6 +1,10 @@
 import * as api from '../api';
 import * as actions from './actions';
 
+////////////////////////////////////////////////////////////////////////////////
+/// Initial state
+////////////////////////////////////////////////////////////////////////////////
+
 const initialState = {
   isAuthenticated: api.isAuthenticated(),
   words: [],
@@ -8,117 +12,162 @@ const initialState = {
   searchPhrasesText: '',
 };
 
+
+////////////////////////////////////////////////////////////////////////////////
+/// Auth
+////////////////////////////////////////////////////////////////////////////////
+
+const setAuthenticated = (state, action) => ({
+  ...state,
+  isAuthenticated: true
+});
+
+const unsetAuthenticated = (state, action) => ({
+  ...state,
+  isAuthenticated: false
+});
+
+
+////////////////////////////////////////////////////////////////////////////////
+/// Words
+////////////////////////////////////////////////////////////////////////////////
+
+const setWords = (state, action) => ({
+  ...state,
+  words: action.data.words
+});
+
+const editWord = (state, action) => {
+  const words = state.words.map(w => {
+    if (w.id === action.data.id) {
+      w.text = action.data.text;
+      w.image = action.data.image;
+      w.notes = action.data.notes;
+      w.gender = action.data.gender;
+    }
+    return w;
+  });
+  return {
+    ...state,
+    words
+  };
+};
+
+const deleteWord = (state, action) => {
+  const words = state.words.filter(w => w.id !== action.data.id);
+  return {
+    ...state,
+    words
+  };
+};
+
+
+////////////////////////////////////////////////////////////////////////////////
+/// Phrases
+////////////////////////////////////////////////////////////////////////////////
+
+const setPhrases = (state, action) => ({
+  ...state,
+  phrases: action.data.phrases
+});
+
+const editPhrase = (state, action) => {
+  const phrases = state.phrases.map(p => {
+    if (p.id === action.data.id) {
+      p.text = action.data.text;
+      p.image = action.data.image;
+      p.notes = action.data.notes;
+    }
+    return p;
+  });
+  return {
+    ...state,
+    phrases
+  };
+};
+
+const deletePhrase = (state, action) => {
+  const phrases = state.phrases.filter(p => p.id !== action.data.id);
+  return {
+    ...state,
+    phrases
+  };
+};
+
+const addRelatedWord = (state, action) => {
+  const phrases = state.phrases.map(p => {
+    if (p.id === action.data.phraseId) {
+      const word = {
+        id: action.data.wordId,
+        text: action.data.wordText,
+      };
+      if (Array.isArray(p.related_words)) {
+        const words = p.related_words.filter(w => w.id !== action.data.wordId);
+        words.push(word);
+        p.related_words = words;
+      }
+      else {
+        p.related_words = [word];
+      }
+    }
+    return p;
+  });
+  return {
+    ...state,
+    phrases
+  };
+};
+
+const removeRelatedWord = (state, action) => {
+  const phrases = state.phrases.map(p => {
+    if (p.id === action.data.phraseId && Array.isArray(p.related_words)) {
+      const words = p.related_words.filter(w => w.id !== action.data.wordId);
+      p.related_words = words;
+    }
+    return p;
+  });
+  return {
+    ...state,
+    phrases
+  };
+};
+
+const setSearchPhrasesText = (state, action) => {
+  return {
+    ...state,
+    searchPhrasesText: action.data.searchPhrasesText
+  };
+};
+
+
+////////////////////////////////////////////////////////////////////////////////
+/// Reducer
+////////////////////////////////////////////////////////////////////////////////
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
   case actions.SET_AUTHENTICATED:
-    return {
-      ...state,
-      isAuthenticated: true
-    };
-
+    return setAuthenticated(state, action);
   case actions.UNSET_AUTHENTICATED:
-    return {
-      ...state,
-      isAuthenticated: false
-    };
-
+    return unsetAuthenticated(state, action);
   case actions.SET_WORDS:
-    return {
-      ...state,
-      words: action.data.words
-    };
-
+    return setWords(state, action);
   case actions.EDIT_WORD:
-    const words = state.words.map(w => {
-      if (w.id === action.data.id) {
-        w.text = action.data.text;
-        w.image = action.data.image;
-        w.notes = action.data.notes;
-        w.gender = action.data.gender;
-      }
-      return w;
-    });
-    return {
-      ...state,
-      words
-    };
-
+    return editWord(state, action);
   case actions.DELETE_WORD:
-    const filteredWords = state.words.filter(w => w.id !== action.data.id);
-    return {
-      ...state,
-      words: filteredWords
-    };
-
+    return deleteWord(state, action);
   case actions.SET_PHRASES:
-    return {
-      ...state,
-      phrases: action.data.phrases
-    };
-
+    return setPhrases(state, action);
   case actions.EDIT_PHRASE:
-    const phrases = state.phrases.map(p => {
-      if (p.id === action.data.id) {
-        p.text = action.data.text;
-        p.image = action.data.image;
-        p.notes = action.data.notes;
-      }
-      return p;
-    });
-    return {
-      ...state,
-      phrases
-    };
-
+    return editPhrase(state, action);
   case actions.DELETE_PHRASE:
-    const filteredPhrases = state.phrases.filter(p => p.id !== action.data.id);
-    return {
-      ...state,
-      phrases: filteredPhrases
-    };
-
+    return deletePhrase(state, action);
   case actions.ADD_RELATED_WORD:
-    const updPhrases = state.phrases.map(p => {
-      if (p.id === action.data.phraseId) {
-        const word = {
-          id: action.data.wordId,
-          text: action.data.wordText,
-        };
-        if (Array.isArray(p.related_words)) {
-          const words = p.related_words.filter(w => w.id !== action.data.wordId);
-          words.push(word);
-          p.related_words = words;
-        }
-        else {
-          p.related_words = [word];
-        }
-      }
-      return p;
-    });
-    return {
-      ...state,
-      phrases: updPhrases
-    };
-
+    return addRelatedWord(state, action);
   case actions.REMOVE_RELATED_WORD:
-    const updatedPhrases = state.phrases.map(p => {
-      if (p.id === action.data.phraseId && Array.isArray(p.related_words)) {
-        const words = p.related_words.filter(w => w.id !== action.data.wordId);
-        p.related_words = words;
-      }
-      return p;
-    });
-    return {
-      ...state,
-      phrases: updatedPhrases
-    };
-
+    return removeRelatedWord(state, action);
   case actions.SET_SEARCH_PHRASES_TEXT:
-    return {
-      ...state,
-      searchPhrasesText: action.data.searchPhrasesText
-    };
-
+    return setSearchPhrasesText(state, action);
   default:
     return state;
   }
